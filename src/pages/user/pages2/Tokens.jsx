03fd149504d4f628e694541d07d5df5e5d3278de
PatoCst <pa.castroj@duocuc.cl>
2025-12-10 17:24:31 -0300

    idk tokens update i think

--- src/pages/user/pages2/Tokens.jsx
+++ src/pages/user/pages2/Tokens.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../../../components/atoms/Button";
+import "../../../styles/pages/Tokens.css";
+import "../../../../public/img/profe.jpg"
 
 function Tokens() {
+  const [showImage, setShowImage] = useState(false);
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>¿Donde se pueden ver los tokens?</h1>
+    <div className="tokens-container">
+      <h1>¿Cómo se hacen los Tokens?</h1>
+      <h2>Según Byron Araya:</h2>
 
       <img
+        className="tokens-image"
         src="https://pbs.twimg.com/media/EQdATjFXUAAVY_E.jpg"
         alt="Imagen de token"
-        style={{
-          width: "300px",
-          height: "auto",
-          borderRadius: "8px"
-        }}
       />
+
+      <Button
+        variant="primary"
+        onClick={() => setShowImage(true)}
+        className="tokens-button"
+      >
+        Resultados!
+      </Button>
+
+      {showImage && (
+        <img
+          className="tokens-image result-image"
+          src= "../../../../public/img/profe.jpg"
+          alt="Resultado"
+        />
+      )}
     </div>
   );
 }
 
-export default Tokens
+export default Tokens;
